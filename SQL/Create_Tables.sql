12e75610226a9b5b5751f90968c061dc13db13c9
Amal-Alawour <68445941+Amal-Alawour@users.noreply.github.com>
2026-08-06 03:17:26 +0300

    Update Create_Tables.sql

--- SQL/Create_Tables.sql
+++ SQL/Create_Tables.sql
@@ -1,1 +1,31 @@
+CREATE DATABASE Sales_Analytics;
+GO
 
+USE Sales_Analytics;
+GO
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(50),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Sales (
+    SaleID INT PRIMARY KEY,
+    CustomerID INT,
+    ProductID INT,
+    SalesDate DATE,
+    Quantity INT,
+    TotalAmount DECIMAL(10,2),
+
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
