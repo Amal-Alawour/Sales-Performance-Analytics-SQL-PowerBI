553f7fe007c8dfb856c3571e6d97fa0de0204144
Amal-Alawour <68445941+Amal-Alawour@users.noreply.github.com>
2026-08-06 03:18:34 +0300

    Update Analysis_Queries.sql

--- SQL/Analysis_Queries.sql
+++ SQL/Analysis_Queries.sql
@@ -1,1 +1,57 @@
+USE Sales_Analytics;
+GO
 
+-- Total Sales
+SELECT 
+    SUM(TotalAmount) AS TotalSales
+FROM Sales;
+
+
+-- Total Quantity Sold
+SELECT 
+    SUM(Quantity) AS TotalQuantity
+FROM Sales;
+
+
+-- Sales by Customer
+SELECT
+    c.CustomerName,
+    SUM(s.TotalAmount) AS TotalSales
+FROM Sales s
+JOIN Customers c
+ON s.CustomerID = c.CustomerID
+GROUP BY c.CustomerName
+ORDER BY TotalSales DESC;
+
+
+-- Sales by City
+SELECT
+    c.City,
+    SUM(s.TotalAmount) AS TotalSales
+FROM Sales s
+JOIN Customers c
+ON s.CustomerID = c.CustomerID
+GROUP BY c.City
+ORDER BY TotalSales DESC;
+
+
+-- Top Products
+SELECT
+    p.ProductName,
+    SUM(s.TotalAmount) AS TotalSales
+FROM Sales s
+JOIN Products p
+ON s.ProductID = p.ProductID
+GROUP BY p.ProductName
+ORDER BY TotalSales DESC;
+
+
+-- Sales by Category
+SELECT
+    p.Category,
+    SUM(s.TotalAmount) AS TotalSales
+FROM Sales s
+JOIN Products p
+ON s.ProductID = p.ProductID
+GROUP BY p.Category
+ORDER BY TotalSales DESC;
